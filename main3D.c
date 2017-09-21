ff86fbd2bcfdaa960820457a8d53e81afdfd2a62
Walesey <joshua.wales@gmail.com>
2017-09-21 14:46:13 +1000

    Add shared memory for piping data to the renderer.

--- main3D.c
+++ main3D.c
@@ -4,66 +4,75 @@
 #include <GL/glut.h>
 #endif
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 #include <fcntl.h>
-// #include <pthread.h>
+#include <pthread.h>
+#include <sys/mman.h>
 
 #include "renderers/renderer3D.h"
 #include "renderers/textfile.h"
 #include "binary/binary.h"
 
 
-#define READ_BUFFER 256
-#define IMAGE_WIDTH 32
+#define READ_BUFFER 100000
+#define IMAGE_WIDTH 128
 #define FRAMES_PER_UPDATE 10
 
 int updateCounter = 0;
+int running = 1;
 
-// pthread_mutex_t lock;
+int nbBytes;
+unsigned char *imageData;
+Image img;
 
-void initImageFile() {
-  File f;
-  f.size = sizeof(unsigned char)*IMAGE_WIDTH*IMAGE_WIDTH*IMAGE_WIDTH*4;
-  f.data = malloc(f.size);
-  fileWrite("image", f);
-  free(f.data);
+pid_t pid;
+pthread_mutex_t lock;
+
+void initImageData() {
+  nbBytes = sizeof(unsigned char)*IMAGE_WIDTH*IMAGE_WIDTH*IMAGE_WIDTH*4;
+  int protection = PROT_READ | PROT_WRITE;
+  int visibility = MAP_ANONYMOUS | MAP_SHARED;
+  imageData = mmap(NULL, nbBytes, protection, visibility, 0, 0);
+  img.data = malloc(nbBytes);
+  img.width = IMAGE_WIDTH;
+  img.height = IMAGE_WIDTH;
+  img.depth = IMAGE_WIDTH;
 }
 
 void readInputData() {
   int filed = open("data", O_RDWR );
   char buf[READ_BUFFER];
 
-  if( !filed ) {
+  if (!filed) {
     printf("Openfile error\n");
     exit(-1);
   }
 
   int nbytes;
-  while(1) {
+  while (running) {
     nbytes = read(filed, buf, READ_BUFFER);
     printf("read %d bytes from file.\n", nbytes);
     if(nbytes > 0) {
-      File f = fileRead("image");
+      pthread_mutex_lock(&lock);
       int intensity = 1000/cbrt(nbytes);
-      linear3DVisualisation(buf, f.data, nbytes, IMAGE_WIDTH, intensity);
-      fileWrite("image", f);
-      free(f.data);
+      linear3DVisualisation(buf, imageData, nbytes, IMAGE_WIDTH, intensity);
+      pthread_mutex_unlock(&lock);
     }
     usleep(100*1000);
   }
 }
 
 void renderUpdate() {
+  usleep(15000);
   updateCounter--;
   if (updateCounter <=0) {
-    File f = fileRead("image");
     updateCounter = FRAMES_PER_UPDATE;
-    Image img;
-    img.data = f.data;
-    img.width = IMAGE_WIDTH;
-    img.height = IMAGE_WIDTH;
-    img.depth = IMAGE_WIDTH;
+    pthread_mutex_lock(&lock);
+    memcpy(img.data, imageData, nbBytes);
     setImage(img);
+    pthread_mutex_unlock(&lock);
   }
 }
 
@@ -71,23 +80,32 @@ void launchWindow(int argc, char **argv) {
   glutInit(&argc, argv);
   glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH | GLUT_3_2_CORE_PROFILE);
   glutCreateWindow("Bin Pipe 3D");
-  glutReshapeWindow(800, 800);
+  glutReshapeWindow(400, 400);
   initRenderer();
   onUpdate(renderUpdate);
   glutMainLoop();
 }
 
 int main(int argc, char **argv) {
-  initImageFile();
-  pid_t pid;
-  if ((pid = fork()) < 0){
-      fprintf(stderr, "Failed to fork()!\n");
-      exit(2);
+  initImageData();
+
+  if (pthread_mutex_init(&lock, NULL) != 0) {
+    printf("\n mutex init failed\n");
+    return 1;
   }
+
+  if ((pid = fork()) < 0){
+    fprintf(stderr, "Failed to fork()!\n");
+    return 1;
+  }
+
   if (pid == 0) { // child
     readInputData();
   } else { // parent
     launchWindow(argc, argv);
   }
+
+  pthread_mutex_destroy(&lock);
+  running = 0;
   return 0;
 }
